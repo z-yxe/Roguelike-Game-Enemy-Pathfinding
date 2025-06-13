15364f3ed8841349cb73a373695672a7b7fdabea
Tazakka Atthariq <145776689+z-yxe@users.noreply.github.com>
2025-06-13 22:13:33 +0700

    Update Dokumentasi

--- Player/PickableWeapon.cs
+++ Player/PickableWeapon.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 
 public class PickableWeapon : MonoBehaviour
 {
-    public string weaponType; // "Gun" atau "Sword"
+    public string weaponType;
     private SpriteRenderer spriteRenderer;
 
+    // Sets sprite layer
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
 
-        // Pastikan weapon selalu di belakang player
         if (spriteRenderer != null)
         {
-            // Nilai negatif akan membuat object render di belakang object dengan nilai yang lebih tinggi
             spriteRenderer.sortingLayerName = "Character";
             spriteRenderer.sortingOrder = -1;
         }
     }
 
+    // Updates weapon sprite
     public void SetWeaponSprite(Sprite weaponSprite)
     {
         if (spriteRenderer != null)
