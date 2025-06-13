0e36ac982d406b4c422821d39e5db4d747756f03
Tazakka Atthariq <145776689+z-yxe@users.noreply.github.com>
2025-06-13 22:21:00 +0700

    Update Dokumentasi

--- Player/WeaponBased.cs
+++ Player/WeaponBased.cs
@@ -5,10 +5,12 @@ public abstract class WeaponBase : MonoBehaviour
     public string weaponName;
     protected Animator weaponAnimator;
 
+    // Gets weapon animator
     protected virtual void Awake()
     {
         weaponAnimator = GetComponent<Animator>();
     }
 
+    // Defines attack method
     public abstract void Attack();
 }
